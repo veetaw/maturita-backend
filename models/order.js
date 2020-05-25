55a8c88ec18221e08b7079c7f12e013f85ca9011
vito <vitopiegari01@gmail.com>
2020-05-26 01:15:28 +0200

    Make order total auto calculated

--- models/order.js
+++ models/order.js
@@ -14,22 +14,35 @@ Order.init({
         primaryKey: true
     },
     start: {
-        type: DataTypes.DATE, allowNull :false,
+        type: DataTypes.DATE,
+        allowNull: false,
     },
     shipped: {
         type: DataTypes.BOOLEAN,
-        allowNull :true,
-        defaultValue :false,
+        allowNull: true,
+        defaultValue: false,
     },
     delivered: {
         type: DataTypes.BOOLEAN,
-        allowNull :true,
-        defaultValue :false,
+        allowNull: true,
+        defaultValue: false,
     },
     total: {
-        type: DataTypes.FLOAT,
-        allowNull :false,
-    },
+        type: DataTypes.VIRTUAL,
+        get() {
+            const p = new Promise((resolve, reject) => {
+                this.getItems().then(items => {
+                    var total = 0.0;
+                    items.forEach(item => {
+                        total += item.price;
+                    });
+                    resolve(total);
+                });
+            });
+
+            return p;
+        }
+    }
 }, { sequelize, modelName: 'order' });
 
 Order.belongsTo(Pizzeria, { targetKey: 'id' });
@@ -37,3 +50,17 @@ Order.belongsTo(User, { targetKey: 'id' });
 Order.hasMany(Item, { targetKey: 'id' });
 
 module.exports = Order;
+
+function getMethods(obj) {
+    var result = [];
+    for (var id in obj) {
+        try {
+            if (typeof (obj[id]) == "function") {
+                result.push(id + ": " + obj[id].toString());
+            }
+        } catch (err) {
+            result.push(id + ": inaccessible");
+        }
+    }
+    return result;
+}
