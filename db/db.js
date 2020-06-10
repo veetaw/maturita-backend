e3f79cb2232a949534050f92c1b0b3ff5ef9f8c8
vito <vitopiegari01@gmail.com>
2020-06-10 20:48:54 +0200

    Fix crash

--- db/db.js
+++ db/db.js
@@ -3,9 +3,9 @@ const config = require('../config')
 
 // TODO: prod use mysql
 if (config.env != 'prod')
-    const sequelize = new Sequelize('sqlite:db.sqlite')
+    var sequelize = new Sequelize('sqlite:db.sqlite')
 else
-    const sequelize = new Sequelize(config.db.db_name, config.db.username, config.db.password, {
+    var sequelize = new Sequelize(config.db.db_name, config.db.username, config.db.password, {
         dialect: 'mysql',
         host: config.db.url,
     })
